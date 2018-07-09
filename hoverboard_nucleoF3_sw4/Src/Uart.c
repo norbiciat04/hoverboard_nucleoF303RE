4e374a09f22e92975689a462d99bd405230b8333
norbiciat04 <norbiciat04@gmail.com>
2018-07-09 22:54:32 +0200

    Add several Uart command for PID. Add comment sending by Uart to each command.

--- hoverboard_nucleoF3_sw4/Src/Uart.c
+++ hoverboard_nucleoF3_sw4/Src/Uart.c
@@ -4,9 +4,22 @@
  *  Created on: 17.06.2018
  *      Author: Norbi
  */
-
+#include <stdlib.h>
 #include "Uart.h"
 #include "BLDC_Motors.h"
+#include "PID_regulator.h"
+
+
+extern UART_HandleTypeDef huart2;
+
+uint8_t str[50];
+uint16_t size;
+
+extern double Kp;
+extern double Ki;
+extern double Kd;
+
+extern uint8_t allow;
 
 void Uart_Receive (void) {
 
@@ -42,30 +55,69 @@ void comand_recognition(char *input_comand) {
 	case 1:		//command without arguments
 		if (strcmp(cr_array[0], "MOT_STOP") == 0) {
 			Stop_LR_Motors();
+			size = sprintf(str, "Stop motors\r\n");
+			HAL_UART_Transmit(&huart2, str, size, 1000);
+		}
+		if (strcmp(cr_array[0], "A") == 0) {
+			allow=1;
+			size = sprintf(str, "Allow=%d\r\n", allow);
+			HAL_UART_Transmit(&huart2, str, size, 1000);
+		}
+		if (strcmp(cr_array[0], "NA") == 0) {
+			allow=0;
+			size = sprintf(str, "Allow=%d\r\n", allow);
+			HAL_UART_Transmit(&huart2, str, size, 1000);
 		}
 		break;
 
 	case 2:		//one argument command
 		if (strcmp(cr_array[0], "L_PWM") == 0) {
 			Set_Left_Motor_Speed(atoi(cr_array[1]));
+			size = sprintf(str, "Left motor PWM=%d\r\n", atoi(cr_array[1]));
+			HAL_UART_Transmit(&huart2, str, size, 1000);
 		}
 		if (strcmp(cr_array[0], "R_PWM") == 0) {
 			Set_Right_Motor_Speed(atoi(cr_array[1]));
+			size = sprintf(str, "Right motor PWM=%d\r\n", atoi(cr_array[1]));
+			HAL_UART_Transmit(&huart2, str, size, 1000);
 		}
 		if (strcmp(cr_array[0], "L_DIR") == 0) {
 			Set_Left_Motor_Dir(atoi(cr_array[1]));
+			size = sprintf(str, "Left motor dir=%d\r\n", atoi(cr_array[1]));
+			HAL_UART_Transmit(&huart2, str, size, 1000);
 		}
 		if (strcmp(cr_array[0], "R_DIR") == 0) {
 			Set_Right_Motor_Dir(atoi(cr_array[1]));
+			size = sprintf(str, "Right motor dir=%d\r\n", atoi(cr_array[1]));
+			HAL_UART_Transmit(&huart2, str, size, 1000);
 		}
 		if (strcmp(cr_array[0], "MOT_DIR") == 0) {
 			Set_LR_Motors_Dir(atoi(cr_array[1]));
+			size = sprintf(str, "Motors dir=%d\r\n", atoi(cr_array[1]));
+			HAL_UART_Transmit(&huart2, str, size, 1000);
+		}
+		if (strcmp(cr_array[0], "P") == 0) {
+			Kp=atof(cr_array[1]);
+			size = sprintf(str, "Kp=%s\r\n", cr_array[1]);
+			HAL_UART_Transmit(&huart2, str, size, 1000);
+		}
+		if (strcmp(cr_array[0], "I") == 0) {
+			Ki=atof(cr_array[1]);
+			size = sprintf(str, "Ki=%s\r\n", cr_array[1]);
+			HAL_UART_Transmit(&huart2, str, size, 1000);
+		}
+		if (strcmp(cr_array[0], "D") == 0) {
+			Kd=atof(cr_array[1]);
+			size = sprintf(str, "Kd=%s\r\n", cr_array[1]);
+			HAL_UART_Transmit(&huart2, str, size, 1000);
 		}
 		break;
 
 	case 3:		//two arguments command
 		if (strcmp(cr_array[0], "MOT_PWM") == 0) {
 			Set_LR_Motors_Speed(atoi(cr_array[1]), atoi(cr_array[2]));
+			size = sprintf(str, "Motor PWM: left=%d right=%d\r\n", atoi(cr_array[1]), atoi(cr_array[2]));
+			HAL_UART_Transmit(&huart2, str, size, 1000);
 		}
 		break;
 
